1e521b9ff63c4244fe4ff4af4a47562f4010c9b8
austinsipp <austin.sipp@gmail.com>
2023-04-19 19:56:50 -0700

    add new bread form and back buttons

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 
 
 
 app.get('/', (req,res) => {
     res.send('Welcome to an Awesome App about Bread!')
+    //res.render('index')
 })
 
 const breadsController = require('./controllers/breads_controller.js')
