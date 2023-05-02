a658c2a00b1eba6ac8c6d551d2852075f0571c36
austinsipp <austin.sipp@gmail.com>
2023-05-02 11:18:42 -0700

    finish implementing breads and bakers with mongodb

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,23 +2,25 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 const seedData = require('../models/seed.js')
+const Baker = require('../models/baker.js')
 
 
 
-// INDEX
+// Index:
 breads.get('/', (req, res) => {
-    Bread.find()
+  Baker.find()
+    .then(foundBakers => {
+      Bread.find()
       .then(foundBreads => {
-        res.render('Index',
-      {
-        breads: foundBreads,
-        title: 'Index Page'
-      }
-    )
+          res.render('index', {
+              breads: foundBreads,
+              bakers: foundBakers,
+              title: 'Index Page'
+          })
       })
-    
-  // res.send(Bread)
+    })
 })
+
 
 //seed
 breads.get('/data/seed', (req, res) => {
@@ -34,33 +36,44 @@ breads.get('/data/seed', (req, res) => {
 
 // NEW
 breads.get('/new', (req, res) => {
-  res.render('new')
+  Baker.find()
+  .then(foundBakers => {
+    res.render('new', {
+      bakers: foundBakers
+    })
+  })
 })
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-  Bread.findById(req.params.id) 
-    .then(foundBread => { 
-      res.render('edit', {
-        bread: foundBread 
-      })
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers 
+            })
+          })
     })
 })
+
 
 
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  Bread.findById(req.params.arrayIndex)
-    .then(foundBread => {
-      res.render('show', {
-        bread: foundBread
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .populate('baker')
+      .then(foundBread => {
+        const bakedBy = foundBread.getBakedBy() 
+        console.log(bakedBy)
+        res.render('show', {
+            bread: foundBread
+        })
       })
     })
-    .catch(err => {
-      res.send('404')
-    })
-})
+
 
 
 
@@ -104,7 +117,13 @@ breads.post('/', (req, res) => {
     req.body.hasGluten = false
   }
   Bread.create(req.body)
-  res.redirect('/breads')
+  .then(() => {
+    res.redirect('/breads')
+  })
+  .catch(err => {
+    console.log(err)
+    res.send(err)
+  })
 })
 
 
--- models/bread.js
+++ models/bread.js
@@ -6,8 +6,18 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: 'http://placehold.it/500x500.png' }
+  image: { type: String, default: 'http://placehold.it/500x500.png' },
+  baker: {
+    type: Schema.Types.ObjectID,
+    ref: 'Baker'
+  }
 })
+
+// helper methods 
+breadSchema.methods.getBakedBy = function(){
+  return `${this.name} was baked with love by ${this.baker.name}, who has been with us since ${this.baker.startDate.getFullYear()}`
+}
+
 
 
 
